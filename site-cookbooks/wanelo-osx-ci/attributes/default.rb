f36991ce233a155b2183e764aa827fb90aa7b984
James Hart & Sarah Mei <pair+james+sarah@wanelo.com>
2014-01-07 19:14:47 -0800

    Programattically add the vigorish build.

--- site-cookbooks/wanelo-osx-ci/attributes/default.rb
+++ site-cookbooks/wanelo-osx-ci/attributes/default.rb
@@ -1,1 +1,6 @@
 default['jenkins']['user'] = 'wanelo'
+
+# jenkins cookbook overrides
+node.default['jenkins']['node']['home'] = '/Users/wanelo'
+node.default['jenkins']['server']['host'] = 'localhost'
+node.default['jenkins']['server']['url'] = 'http://localhost:8080'
--- site-cookbooks/wanelo-osx-ci/recipes/default.rb
+++ site-cookbooks/wanelo-osx-ci/recipes/default.rb
@@ -7,3 +7,18 @@ execute 'launch jenkins now' do
   command 'launchctl load ~/Library/LaunchAgents/homebrew.mxcl.jenkins.plist'
   user node['jenkins']['user']
 end
+
+job_name = 'vigorish'
+job_filename = "#{job_name}_config.xml"
+job_config = File.join(node[:jenkins][:node][:home], job_filename)
+
+jenkins_job job_name do
+  action :nothing
+  config job_config
+end
+
+cookbook_file job_config do
+  source    job_filename
+  notifies  :update, resources(:jenkins_job => job_name), :immediately
+  notifies  :build, resources(:jenkins_job => job_name), :immediately
+end
